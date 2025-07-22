1ec021aff1698152e598712d9d54c3700ad2bb42
MinhCreator <92376650+MinhCreator@users.noreply.github.com>
2025-07-22 13:08:44 +0700

    modify

--- build.sh
+++ build.sh
@@ -2,20 +2,34 @@
 
 # Define the project directory
 PROJECT_DIR=$(pwd)
+echo "Project directory: $PROJECT_DIR"
 
 # Define the virtual environment directory
 VENV_DIR=${PROJECT_DIR}/venv
+echo "Virtual environment directory: $VENV_DIR"
 
 # Create a virtual environment if it doesn't exist
+echo "Creating virtual environment in $VENV_DIR please keep waiting..."
 if [ ! -d "$VENV_DIR" ]; then
   python -m venv $VENV_DIR
 fi
 
+# Creating upload folder if it doesn't exist
+UPLOAD_DIR=${PROJECT_DIR}/static/upload_image
+
+if [ ! -d "$UPLOAD_DIR" ]; then
+  mkdir $UPLOAD_DIR
+  echo "upload folder created"
+fi
+
 # Activate the virtual environment
+echo "Activating virtual environment..."
 source $VENV_DIR/bin/activate
 
 # Install dependencies from requirements.txt
+echo "Installing dependencies..."
 pip install -r requirements.txt
 
 # Run the main application
+echo "deploying application..."
 python main.py
--- main.py
+++ main.py
@@ -30,4 +30,4 @@ def serverError(error):
 
 # if run debug mode please replace app.run() by app.run(debug=True)
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
