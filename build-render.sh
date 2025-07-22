364a780088a3bed09b07968e8f9ed71d4783666d
MinhCreator <92376650+MinhCreator@users.noreply.github.com>
2025-07-22 13:14:13 +0700

    add build for render

---
+++ build-render.sh
@@ -0,0 +1,11 @@
+# Creating upload folder if it doesn't exist
+UPLOAD_DIR=${PROJECT_DIR}/static/upload_image
+
+if [ ! -d "$UPLOAD_DIR" ]; then
+  mkdir $UPLOAD_DIR
+  echo "upload folder created"
+fi
+
+# Run the main application
+echo "deploying application..."
+python3 main.py
